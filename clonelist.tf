4dbf6bc9d9aeda31d889b9ea8ba764719db1da1a
Viktor Bushmin <viktor.bushmin@gmail.com>
2022-05-28 10:38:40 +0300

    minor changes

--- clonelist.tf
+++ clonelist.tf
@@ -10,3 +10,16 @@ git clone ${project.ssh_url_to_repo}%{endfor}
 EOT
   filename   = "cloneprojects.sh"
 }
+
+###########################################
+# save git names list to file
+###########################################
+resource "local_file" "commit_and_push_projects" {
+  depends_on = [data.gitlab_projects.group_projects]
+  content    = <<EOT
+#!/bin/bash
+%{for project in data.gitlab_projects.group_projects.projects}
+cd ${project.path} && git add . && git pull && git commit -m "LiveDoc YAML Updated" && git push && cd ..%{endfor}
+EOT
+  filename   = "commit_and_push_projects.sh"
+}
--- users.tf
+++
@@ -1,5 +0,0 @@
-# Gitlab users objects
-data "gitlab_user" "users" {
-  for_each = toset(var.gitlab_users_list)
-  username = each.value
-}
